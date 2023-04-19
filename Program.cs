6f47cc6eab3700063ed928b18387b39358cfb5b5
Boris Drybin <makoh93@gmail.com>
2023-04-19 20:23:42 +0300

    second task added

--- Program.cs
+++ Program.cs
@@ -4,26 +4,56 @@
 // 12821 -> да
 // 23432 -> да
 
-Console.WriteLine("Введите пятизначное число");
-int num = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите пятизначное число");
+// int num = Convert.ToInt32(Console.ReadLine());
 
-int a = num / 10000;
-int b = num % 10;
-int c = num / 1000 % 10;
-int d = num / 10 % 10;
+// int a = num / 10000;
+// int b = num % 10;
+// int c = num / 1000 % 10;
+// int d = num / 10 % 10;
 
-if (num > 9999 && num < 100000)
-{
-    if (a == b && c == d)
-    {
-    Console.WriteLine("Палиндром");
-    }
-    else
-    {
-    Console.WriteLine("Не палиндром");
-    }
-}
-else
-{
-    Console.WriteLine("Число не пятизначное!");
-}
+// if (num > 9999 && num < 100000)
+// {
+//     if (a == b && c == d)
+//     {
+//     Console.WriteLine("Палиндром");
+//     }
+//     else
+//     {
+//     Console.WriteLine("Не палиндром");
+//     }
+// }
+// else
+// {
+//     Console.WriteLine("Число не пятизначное!");
+// }
+
+
+
+// Напишите программу, которая принимает на вход координаты 
+// двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+Console.WriteLine("Введите x1");
+int x1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите y1");
+int y1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите z1");
+int z1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите x2");
+int x2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите y2");
+int y2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите z2");
+int z2 = Convert.ToInt32(Console.ReadLine());
+
+double d = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2) + Math.Pow((z2 - z1), 2));
+
+Console.WriteLine(d = Math.Round(d, 2));
